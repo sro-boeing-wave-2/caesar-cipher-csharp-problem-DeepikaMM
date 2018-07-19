3c8cadd54b19c54e264054ed71939abe392d0b62
Deepika Mohan <deepikamm12@gmail.com>
2018-07-19 09:19:46 +0530

    updated

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,23 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string RotatedString = "";
+            for(int i=0;i<text.Length;i++)
+            {
+                int test = (int)text[i];
+                if (test >= 65 && test <= 90)
+                {
+                    RotatedString += (char)((int)((test + shiftKey-65) % 26+65));
+                }
+                else if (test >= 97 && test <= 122)
+                {
+                    RotatedString += (char)((int)((test + shiftKey - 97) % 26 + 97));
+                }
+                else
+                    RotatedString += text[i];
+            }
+            return RotatedString;
+           
         }
     }
 }
